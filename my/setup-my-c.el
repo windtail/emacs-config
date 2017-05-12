c6d1e5fa66281bc6e4b79885463e7cf029178cc7
Luo Jiejun <ljj@knd.msn>
2017-05-12 13:52:11 +0800

    from gtags to ymcd (incomplete)

--- my/setup-my-c.el
+++ my/setup-my-c.el
@@ -1,33 +1,25 @@
 
 (require 'setup-my-auto-install)
 
-(auto-install '(counsel-gtags))
+(auto-install '(ycmd company-ycmd flycheck-ycmd))
 
 (require 'setup-my-prog-common)
 
-(defun my-on-c-mode ()
-  (counsel-gtags-mode)
-  (hs-minor-mode)
-  (setq company-backend '((company-dabbrev-code company-gtags)))
-  ;; gnu, k&r, bsd, whitesmith, stroustrup, ellemtel, linux, python, java, user
-  (setq c-default-style "linux")
+(defun my-gdb-config ()
   (defvar gdb-many-windows t)
   (defvar gdb-show-main t)
-  (define-key c-mode-map (kbd "<f5>") 'gdb)
+  (define-key c-mode-map (kbd "<f5>") 'gdb))
+
+(defun my-on-c-mode ()
+  (hs-minor-mode)
+  (setq company-backend '((company-dabbrev-code company-ycmd)))
+  ;; gnu, k&r, bsd, whitesmith, stroustrup, ellemtel, linux, python, java, user
+  (setq c-default-style "linux")
+  (my-gdb-config)
+  (yas-minor-mode 1)
   )
 
 (add-hook 'c-mode-hook 'my-on-c-mode)
-
-(if (eq system-type 'windows-nt)
-  nil
-  (setenv "GTAGSLIBPATH" (concat "/usr/include" ":" "/usr/local/include"))
-  (setenv "MAKEOBJDIRPREFIX" (file-truename "~/.gtags"))
-  )
-
-(with-eval-after-load 'counsel-gtags
-  (define-key counsel-gtags-mode-map (kbd "M-t") 'counsel-gtags-find-definition)
-  (define-key counsel-gtags-mode-map (kbd "M-r") 'counsel-gtags-find-reference)
-  (define-key counsel-gtags-mode-map (kbd "M-s") 'counsel-gtags-find-symbol)
-  (define-key counsel-gtags-mode-map (kbd "M-,") 'counsel-gtags-go-backward))
+(add-hook 'c++-mode-hook 'my-on-c-mode)
 
 (provide 'setup-my-c)
