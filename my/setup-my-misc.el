380994f8337b69c6cc84627e9034aebe9c891acf
Luo Jiejun <ljj@knd.msn>
2017-06-02 14:20:05 +0800

    restclient company config

--- my/setup-my-misc.el
+++ my/setup-my-misc.el
@@ -3,4 +3,7 @@
 
 (auto-install '(restclient company-restclient))
 
+(add-hook 'restclient-mode-hook
+          (lambda () (setq company-backends '(company-restclient company-dabbrev))))
+
 (provide 'setup-my-misc)
