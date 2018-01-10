8c8429b09641b13c4f47912a8b8de523d0f144ef
luojiejun <6020100326ljj@163.com>
2018-01-10 14:29:10 +0800

    Add function to generate pdf using pandoc from reST (support Chinese)

--- my/setup-my-text.el
+++ my/setup-my-text.el
@@ -24,4 +24,14 @@
 
 (add-hook 'org-mode-hook 'yas-minor-mode)
 
+(if (eq system-type 'windows-nt)
+    (setq rst-normal-font "SimSun" rst-bold-font "SimHei")
+  (setq rst-normal-font "AR PL UMing CN" rst-bold-font "文泉驿正黑"))
+
+(defun my-pandoc-rst2pdf ()
+  "convert rst to pdf using pandoc(xelatex)"
+  (interactive)
+  (compile (format "pandoc -t latex --latex-engine=xelatex -s -VCJKoptions=BoldFont=\"%s\" -VCJKmainfont=\"%s\" -o %s.pdf %s"
+                   rst-bold-font rst-normal-font (file-name-base (buffer-name)) (buffer-name))))
+
 (provide 'setup-my-text)
